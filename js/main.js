b205b972d5ba256a395846debd668d2e7a772970
trukatuze <trukatuze@gmail.com>
2018-05-15 14:34:59 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$('.burger').click(function() {
+    $('.navigation-mobile').addClass('active');
+});
+$('.navigation-close').click(function() {
+    $('.navigation-mobile').removeClass('active');
+});
